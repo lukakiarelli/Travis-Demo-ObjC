0cb976081dfd814363b1ca29c20f2e2fa46f5b25
Luca Chiarelli <lukakiarelli@gmail.com>
2015-01-03 12:12:21 +0100

    added certificates

---
+++ build/Travis-Demo-ObjC.build/Release-iphoneos/Travis-Demo-ObjC.build/Objects-normal/arm64/AppDelegate.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/lukakiarelli/Documents/workspace/Swift/Travis-Demo-ObjC/Travis-Demo-ObjC/AppDelegate.m \
+  /Users/lukakiarelli/Documents/workspace/Swift/Travis-Demo-ObjC/Travis-Demo-ObjC/AppDelegate.h
---
+++ build/Travis-Demo-ObjC.build/Release-iphoneos/Travis-Demo-ObjC.build/Objects-normal/arm64/ViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/lukakiarelli/Documents/workspace/Swift/Travis-Demo-ObjC/Travis-Demo-ObjC/ViewController.m \
+  /Users/lukakiarelli/Documents/workspace/Swift/Travis-Demo-ObjC/Travis-Demo-ObjC/ViewController.h
